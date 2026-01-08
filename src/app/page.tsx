c2f47b1d7001fbb0a5a67ad0cb151afd3d7f8174
AidarKh24 <aidarkhatr@gmail.com>
2026-01-08 03:09:01 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero";
+import System from "./sections/System";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <System />
     </main>
   );
 }
